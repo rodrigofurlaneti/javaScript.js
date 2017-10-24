fd7f9c08f7ab5c729e34414912ae17c6ee6bf283
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-10-24 10:56:59 -0200

    Create arrayJs.js

---
+++ arrayJs.js
@@ -0,0 +1,23 @@
+// CRIAR ARRAY DE NUMEROS
+var arr = [];
+arr.push(1);
+arr.push(2);
+arr.push(2);
+arr;
+(3) [1, 2, 2]0: 11: 22: 2length: 3__proto__: Array(0)
+
+//CRIAR ARRAY DE STRINGS
+var arr = [];
+arr.push('a');
+arr.push('b');
+arr.push('c');
+arr;
+(3) ["a", "b", "c"]0: "a"1: "b"2: "c"length: 3__proto__: Array(0)
+
+//CRIAR ARRAY DE OBJETOS
+var arr = [];
+arr.push({name: 'Rodrigo'});
+arr.push({name: 'Luiz'});
+arr.push({name: 'Madeira'});
+arr;
+(3) [{…}, {…}, {…}]0: {name: "Rodrigo"}1: {name: "Luiz"}2: {name: "Madeira"}length: 3__proto__: Array(0)
